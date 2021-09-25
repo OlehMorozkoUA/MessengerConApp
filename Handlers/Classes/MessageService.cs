1f28c6676b1a2610043f330b49a85f3a84110a03
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-09-25 21:24:24 +0300

    Added MessageService and UserService.

---
+++ Handlers/Classes/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using Models.Classes;
+
+namespace Handlers.Classes
+{
+    public class MessageService
+    {
+        public MessengerDbContext DbContext { get; set; }
+        public MessageService(MessengerDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+    }
+}
---
+++ Handlers/Classes/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Models.Classes;
+using System.Collections.Generic;
+
+namespace Handlers.Classes
+{
+    public class UserService
+    {
+        public MessengerDbContext DbContext { get; set; }
+        public UserService(MessengerDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+        public void CreateUser(User user)
+        {
+            DbContext.Add(user);
+            DbContext.SaveChanges();
+        }
+    }
+}
