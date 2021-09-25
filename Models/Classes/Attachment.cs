4b412962d62d4068776cfd3d1af90f276027108b
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-09-25 18:47:27 +0300

    Changed fields in class  "Attachment.cs"

--- Models/Classes/Attachment.cs
+++ Models/Classes/Attachment.cs
@@ -5,9 +5,8 @@ namespace Models.Classes
     public class Attachment : IAttachment
     {
         public int Id { get; set; }
-        public byte[] Value { get; set; }
+        public string Value { get; set; }
         public int Size { get; set; }
-        public int MaxSize { get; set; } = 20;
         public int MessageId { get; set; }
     }
 }
--- Models/Interfaces/IAttachment.cs
+++ Models/Interfaces/IAttachment.cs
@@ -3,9 +3,8 @@
     public interface IAttachment
     {
         public int Id { get; set; }
-        public byte[] Value { get; set; }
+        public string Value { get; set; }
         public int Size { get; set; }
-        public int MaxSize { get; set; } 
         public int MessageId { get; set; }
     }
 }
