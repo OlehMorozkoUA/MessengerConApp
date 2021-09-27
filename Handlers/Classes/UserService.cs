1066671a7cc688fe17362e114e4069c4163134a6
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-09-27 13:21:16 +0300

    Created general CRUD<T> for Models.

--- Handlers/Classes/UserService.cs
+++ Handlers/Classes/UserService.cs
@@ -5,13 +5,13 @@ using System.Collections.Generic;
 
 namespace Handlers.Classes
 {
-    public sealed class UserService : UserCRUD, ICRUD<User>
+    public sealed class UserService : CRUD<User>, ICRUD<User>
     {
         private UserService() { }
-        public static bool Create(User user) => Transaction((MessengerDb) => CreateUser(MessengerDb, user));
-        public static List<User> Read() => Transaction((MessengerDb) => ReadUser(MessengerDb));
-        public static bool Update(int id, object newUser) => Transaction((MessengerDb) => UpdateUser(MessengerDb, id, newUser));
-        public static bool Delete(int id) => Transaction((MessengerDb) => DeleteUser(MessengerDb, id));
+        public static bool Create(User user) => Transaction((MessengerDb) => CreateEntity(MessengerDb, user));
+        public static List<User> Read() => Transaction((MessengerDb) => ReadEntity(MessengerDb));
+        public static bool Update(int id, object newUser) => Transaction((MessengerDb) => UpdateEntity(MessengerDb, id, newUser));
+        public static bool Delete(int id) => Transaction((MessengerDb) => DeleteEntity(MessengerDb, id));
         public static dynamic Transaction(Func<MessengerDbContext, object> func)
         {
             using (MessengerDbContext MessengerDb = new MessengerDbContext())
---
+++ Handlers/Interfaces/CRUD.cs
@@ -0,0 +1,89 @@
+﻿using Handlers.Classes;
+using Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Handlers.Interfaces
+{
+    public class CRUD<T> where T: class
+    {
+        protected static bool CreateEntity(MessengerDbContext MessengerDb, T entity)
+        {
+            try
+            {
+                MessengerDb.Add(entity);
+                MessengerDb.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        protected static List<T> ReadEntity(MessengerDbContext MessengerDb)
+        {
+            List<T> entities = new List<T>();
+            try
+            {
+                IEnumerable<T> MessengerDbEntities = GetMessengerDbEntities(MessengerDb, typeof(T).Name.ToLower());
+                entities = Services<T>.ToList(MessengerDbEntities);
+            }
+            catch (Exception ex) { }
+
+            return entities;
+        }
+        protected static bool UpdateEntity(MessengerDbContext MessengerDb, int id, object newUser)
+        {
+            try
+            {
+                T entity = MessengerDb.Find(typeof(T), id) as T;
+
+                PropertyInfo[] properties = entity.GetType().GetProperties();
+                PropertyInfo[] newProperties = newUser.GetType().GetProperties();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    for (int j = 0; j < newProperties.Length; j++)
+                    {
+                        if (properties[i].Name == newProperties[j].Name)
+                        {
+                            properties[i].SetValue(entity, newProperties[j].GetValue(newUser));
+                        }
+                    }
+                }
+
+                MessengerDb.Update(entity);
+                MessengerDb.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        protected static bool DeleteEntity(MessengerDbContext MessengerDb, int id)
+        {
+            try
+            {
+                T entity = MessengerDb.Find(typeof(T), id) as T;
+                MessengerDb.Remove(entity);
+                MessengerDb.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        private static IEnumerable<T> GetMessengerDbEntities(MessengerDbContext MessengerDb, string type)
+            => (type) switch
+            {
+                "attachment" => MessengerDb.Attachments as IEnumerable<T>,
+                "group" => MessengerDb.Groups as IEnumerable<T>,
+                "message" => MessengerDb.Messages as IEnumerable<T>,
+                "user" => MessengerDb.Users as IEnumerable<T>,
+                _ => null
+            };
+    }
+}
