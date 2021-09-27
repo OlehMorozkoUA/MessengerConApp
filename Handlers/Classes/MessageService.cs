b8e5a7e3b15852dd1b21bf6a4acabc65e6e2462f
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-09-27 13:32:30 +0300

    Added CRUD for MessageService.

--- Handlers/Classes/MessageService.cs
+++ Handlers/Classes/MessageService.cs
@@ -1,8 +1,15 @@
-﻿using Models.Classes;
+﻿using Handlers.Interfaces;
+using Models.Classes;
+using System.Collections.Generic;
 
 namespace Handlers.Classes
 {
-    public static class MessageService
+    public sealed class MessageService : CRUD<Message>, ICRUD<Message>
     {
+        private MessageService() { }
+        public static bool Create(Message message) => Transaction((MessengerDb) => CreateEntity(MessengerDb, message));
+        public static List<Message> Read() => Transaction((MessengerDb) => ReadEntity(MessengerDb));
+        public static bool Update(int id, object newUser) => Transaction((MessengerDb) => UpdateEntity(MessengerDb, id, newUser));
+        public static bool Delete(int id) => Transaction((MessengerDb) => DeleteEntity(MessengerDb, id));
     }
 }
